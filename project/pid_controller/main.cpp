8c22ea0043a316ebfff8f8d68479220feb4b1357
Johnson Joseph <johnsonjoseph@gmail.com>
2023-06-17 16:15:28 -0400

    Tuning PID coefficients

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -248,8 +248,8 @@ int main ()
   PID pid_steer = PID();
   PID pid_throttle = PID();
 
-  pid_steer.Init(1, 1, 1, 1.2, -1.2);
-  pid_throttle.Init(1, 1, 1, 1, -1);
+  pid_steer.Init(0.35, 0.002, 0.10, 1.2, -1.2);
+  pid_throttle.Init(0.35, 0.002, 0.10, 1, -1);
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
   {
